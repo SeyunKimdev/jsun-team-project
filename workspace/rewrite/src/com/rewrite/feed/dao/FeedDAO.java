42bf684a51b6fe1b1d7a0b9ea66bf35c17fe3116
WookCoding <dnrwhddla@gmail.com>
2023-03-04 20:50:40 +0900

    fix : feedDAO, feedFrontController, feed.js, feed.jsp

--- workspace/rewrite/src/com/rewrite/feed/dao/FeedDAO.java
+++ workspace/rewrite/src/com/rewrite/feed/dao/FeedDAO.java
@@ -1,7 +1,11 @@
 package com.rewrite.feed.dao;
 
+import java.util.List;
+import java.util.Map;
+
 import org.apache.ibatis.session.SqlSession;
 
+import com.rewrite.feed.domain.FeedDTO;
 import com.rewrite.mybatis.config.MyBatisConfig;
 
 public class FeedDAO {
@@ -10,5 +14,16 @@ public SqlSession sqlSession;
 	public FeedDAO() {
 		sqlSession = MyBatisConfig.getSqlSessionFactory().openSession(true);
 	}
-
+	
+//	피드 리스트 가져오기 
+	public List<FeedDTO> getFeedList(Map<String, Object> pageMap) {
+		return sqlSession.selectList("feed.getFeedList", pageMap);
+	}
+	
+//	피드 총 개수
+	public Long getTotal() {
+		return sqlSession.selectOne("feed.getTotal");
+	}
+	
+//	
 }
